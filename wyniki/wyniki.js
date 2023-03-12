fd3062421636648ec68babe5bb41a5abd5585924
Jakub Osica <jakub.osica9@gmail.com>
2023-03-12 20:38:36 +0100

    strony glosowania i wynikow+admin panel, bez css

---
+++ wyniki/wyniki.js
@@ -0,0 +1,138 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const session = require('express-session');
+const path = require('path');
+
+// Konfiguracja połączenia z bazą danych
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'wybory'
+});
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodelogin'
+});
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
+
+
+// Odczytanie danych z bazy
+connection.query('SELECT * FROM glosy', function (error, results, fields) {
+    if (error) throw error;
+
+    // Utworzenie tablicy z wynikami
+    const data = [];
+    for (let i = 0; i < results.length; i++) {
+        const row = results[i];
+        data.push({ id: row.id_kandydata, imie: row.imie_g, nazwisko: row.nazwisko_g });
+    }
+
+    // Zdefiniowanie ścieżki do pliku HTML
+    app.get('/wyniki', function (req, res) {
+        let adam = 0
+        let jakub = 0
+        let dawid = 0
+        let html = '<html><head><title>WYNIKI</title>';
+        html += '<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>';
+        html += '</head><body><h1>Wyniki</h1><table>';
+        html += '<thead><tr><th>Liczba głosów</th><th>Imię kandydata</th><th>Nazwisko kandydata</th></tr></thead>';
+        html += '<tbody>';
+        data.forEach(function (result) {
+            if (result.id == 1) {
+                adam++
+            } else if (result.id == 2) {
+                jakub++
+            } else {
+                dawid++
+            }
+        });
+        html += '<tr><td>' + adam + '</td><td>' + 'Adam' + '</td><td>' + 'Paska' + '</td></tr>';
+        html += '<tr><td>' + jakub + '</td><td>' + 'Jakub' + '</td><td>' + 'Osica' + '</td></tr>';
+        html += '<tr><td>' + dawid + '</td><td>' + 'Dawid' + '</td><td>' + 'Muras' + '</td></tr>';
+        html += '</tbody></table>';
+
+        // Tworzenie diagramu kołowego
+        html += '<canvas id="myChart"></canvas><script>';
+        html += 'const ctx = document.getElementById("myChart").getContext("2d");';
+        html += 'const myChart = new Chart(ctx, {type: "pie",';
+        html += 'data: {labels: ["Adam", "Jakub", "Dawid"],';
+        html += 'datasets: [{label: "Liczba głosów",';
+        html += 'data: [' + adam + ', ' + jakub + ', ' + dawid + '],';
+        html += 'backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],}]}});</script>';
+        html += '<style>canvas {max-width: 500px;max-height: 500px;}</style>'
+        html += '</body></html>';
+        res.send(html);
+    });
+    app.get('/admin', function (request, response) {
+        // Render login template
+        response.sendFile(path.join(__dirname + '/login.html'));
+    });
+    app.post('/auth', function (request, response) {
+        // Capture the input fields
+        let username = request.body.username;
+        let password = request.body.password;
+        // Ensure the input fields exists and are not empty
+        if (username && password) {
+            // Execute SQL query that'll select the account from the database based on the specified username and password
+            con.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function (error, results, fields) {
+                // If there is an issue with the query, output the error
+                if (error) throw error;
+                // If the account exists
+                if (results.length > 0) {
+                    // Authenticate the user
+                    request.session.loggedin = true;
+                    request.session.username = username;
+                    // Redirect to home page
+                    response.redirect('/home');
+                } else {
+                    response.send('Incorrect Username and/or Password!');
+                }
+                response.end();
+            });
+        } else {
+            response.send('Please enter Username and Password!');
+            response.end();
+        }
+    });
+    app.get('/home', function (request, response) {
+        // If the user is loggedin
+        if (request.session.loggedin) {
+            let html = '<html><head><title>Dane użytkowników</title></head><body><h1>Dane użytkowników</h1><table>';
+            html += '<thead><tr><th>Kandydat</th><th>Imię głosującego</th><th>Nazwisko głosującego</th></tr></thead>';
+            html += '<tbody>';
+            data.forEach(function (result) {
+                if(result.id == 1) {
+                    html += '<tr><td>Adam Paska'
+                } else if (result.id == 2) {
+                    html += '<tr><td>Jakub Osica'
+                } else {
+                    html += '<tr><td>Dawid Muras'
+                }
+                html += '</td><td>' + result.imie + '</td><td>' + result.nazwisko + '</td></tr>';
+            });
+            html += '</tbody></table></body></html>';
+            response.send(html);
+        } else {
+            // Not logged in
+            response.send('Please login to view this page!')
+        }
+        response.end();
+    });
+
+});
+
+// Uruchomienie serwera
+app.listen(3000, function () {
+    console.log('Serwer został uruchomiony na porcie 3000');
+});
